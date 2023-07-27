2c5aca7de20e3535416d2008323792586ecfc646
Istvan Vagasi <istvan_vagasi@epam.com>
2023-07-27 13:52:53 +0200

    Modified test enviroment add bama

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
